a8d70a947b111e986282b957d90773936af33b5d
fengchuan <fengchuan>
2026-03-11 20:48:43 +0000

    11

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/images/html/' : '/',
   plugins: [vue()],
+  build: {
+    outDir: '/root/scorpio/antares_assets/images/html',
+  },
   server: {
     host: true,
     proxy: {
